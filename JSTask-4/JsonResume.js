9825c187aecdf85245e9b5fe93f529d0c591d39b
Brindha3 <brindhasundar2@gmail.com>
2023-09-26 22:29:04 +0530

    JSON-all Loops, JSON-Resume

---
+++ JSTask-4/JsonResume.js
@@ -0,0 +1,57 @@
+let myResume={
+    "basics": {
+      "name": "Jeya Brindha N",
+      "email": "brindhasundar2@gmail.com",
+      "phone": 9688384884,
+      "qualification": "Dcse",
+      "location": {
+        "address": "10/133, Muthukrishnapuram, Chettiyapathu, Tiruchendur",
+        "postalCode": 608203,
+        "city": "Thoothukudi",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+          "github":"https://github.com/Brindha3"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Foxconn Hann Hai Pvt Ltd",
+        "position": "Data Entry Operater",
+        "startDate": "2022-03-23",
+        "endDate": "2023-06-01",
+        "summary": "i am the Data collection and production problem handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "ISBM University",
+        "department": "Diploma Computer Science Engineering",
+        "batch start year": 2023,
+        "batch end year": 2026,
+        "gpa": 2.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Full Stack Developer",
+        "level": "beginer",
+        
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter"
+      }
+    ]
+  }
+  console.log(myResume);
